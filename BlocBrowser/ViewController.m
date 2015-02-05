b58039acda8c8f693995b6a2d4c69fe4a4331fe5
Peter Scheyer <pscheyer@gmail.com>
2015-02-05 17:00:46 +0300

    20150205 1700 qtime added url text field and case for user not typing http

--- BlocBrowser/ViewController.m
+++ BlocBrowser/ViewController.m
@@ -8,9 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController () <UIWebViewDelegate>
+@interface ViewController () <UIWebViewDelegate, UITextFieldDelegate>
 
 @property (nonatomic, strong) UIWebView *webview;
+@property (nonatomic, strong) UITextField *textField;
 
 @end
 
@@ -23,21 +24,58 @@
     self.webview = [[UIWebView alloc] init];
     self.webview.delegate = self;
     
+    self.textField =[[UITextField alloc] init];
+    self.textField.keyboardType = UIKeyboardTypeURL;
+    self.textField.returnKeyType = UIReturnKeyDone;
+    self.textField.autocapitalizationType = UITextAutocapitalizationTypeNone;
+    self.textField.autocorrectionType = UITextAutocorrectionTypeNo;
+    self.textField.placeholder = NSLocalizedString(@"Website URL", @"Placeholder text for web browser URL Field");
+    self.textField.backgroundColor = [UIColor colorWithWhite:220/255.0f alpha:1];
+    self.textField.delegate = self;
+    
     [mainView addSubview:self.webview];
+    [mainView addSubview:self.textField];
     self.view = mainView;
 }
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.edgesForExtendedLayout = UIRectEdgeNone;
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void) viewWillLayoutSubviews {
     [super viewWillLayoutSubviews];
     
-    //make the webview fill the main view
-    self.webview.frame = self.view.frame;
+//first, calculate some dimensions
+    static const CGFloat itemHeight = 50;
+    CGFloat width = CGRectGetWidth(self.view.bounds);
+    CGFloat browserHeight = CGRectGetHeight(self.view.bounds)- itemHeight;
     
+    //now, assign the frames
+    self.textField.frame = CGRectMake(0, 0, width, itemHeight);
+    self.webview.frame = CGRectMake(0, CGRectGetMaxY(self.textField.frame), width, browserHeight);
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [textField resignFirstResponder];
     
+    NSString *URLString = textField.text;
+    
+    NSURL *URL = [NSURL URLWithString:URLString];
+    
+    if (!URL.scheme) {
+        //the user didn't type http: or https:
+        URL = [NSURL URLWithString:[NSString stringWithFormat:@"http://%@", URLString]];
+    }
+    
+    if (URL) {
+        NSURLRequest *request = [NSURLRequest requestWithURL:URL];
+        [self.webview loadRequest:request];
+    }
+    return NO;
 }
 
 
