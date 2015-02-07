6ed41a98c69363cb45eb7e0584caf052e3926ca5
Peter Scheyer <pscheyer@gmail.com>
2015-02-07 14:11:45 +0300

    20150207 1411qtime functional web browser w buttons search function and auto http add.

--- BlocBrowser/ViewController.m
+++ BlocBrowser/ViewController.m
@@ -12,11 +12,19 @@
 
 @property (nonatomic, strong) UIWebView *webview;
 @property (nonatomic, strong) UITextField *textField;
+@property (nonatomic, strong) UIButton *backButton;
+@property (nonatomic, strong) UIButton *forwardButton;
+@property (nonatomic, strong) UIButton *stopButton;
+@property (nonatomic, strong) UIButton *reloadButton;
+@property (nonatomic, strong) UIActivityIndicatorView *activityIndicator;
+
+@property (nonatomic, assign) NSUInteger frameCount;
 
 @end
 
 @implementation ViewController
 
+#pragma mark - UIViewController
 
 -(void)loadView {
     UIView *mainView = [UIView new];
@@ -33,8 +41,35 @@
     self.textField.backgroundColor = [UIColor colorWithWhite:220/255.0f alpha:1];
     self.textField.delegate = self;
     
-    [mainView addSubview:self.webview];
-    [mainView addSubview:self.textField];
+    self.backButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [self.backButton setEnabled:NO];
+    
+    self.forwardButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [self.forwardButton setEnabled:NO];
+    
+    self.stopButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [self.stopButton setEnabled:NO];
+    
+    self.reloadButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [self.reloadButton setEnabled:NO];
+    
+    [self.backButton setTitle:NSLocalizedString(@"Back", @"Back Command") forState:UIControlStateNormal];
+    [self.backButton addTarget:self.webview action:@selector(goBack) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.forwardButton setTitle:NSLocalizedString(@"Forward", @"Forward Command") forState:UIControlStateNormal];
+    [self.forwardButton addTarget:self.webview action:@selector(goForward) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.stopButton setTitle:NSLocalizedString(@"Stop", @"Stop Command") forState:UIControlStateNormal];
+    [self.stopButton addTarget:self.webview action:@selector(stopLoading) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.reloadButton setTitle:NSLocalizedString(@"Reload", @"Reload Command") forState:UIControlStateNormal];
+    [self.reloadButton addTarget:self.webview action:@selector(reload) forControlEvents:UIControlEventTouchUpInside];
+
+    
+    for (UIView *viewToAdd in @[self.webview, self.textField, self.backButton, self.forwardButton, self.stopButton, self.reloadButton]) {
+        [mainView addSubview:viewToAdd];
+    }
+    
     self.view = mainView;
 }
 
@@ -43,7 +78,12 @@
     [super viewDidLoad];
     
     self.edgesForExtendedLayout = UIRectEdgeNone;
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.activityIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:self.activityIndicator];
+    
+    //test for animator spinner. Also, terrifyingly annoying for a single line of code. Your reactions are not under your control!
+//    [self.activityIndicator startAnimating];
 }
 
 - (void) viewWillLayoutSubviews {
@@ -52,12 +92,24 @@
 //first, calculate some dimensions
     static const CGFloat itemHeight = 50;
     CGFloat width = CGRectGetWidth(self.view.bounds);
-    CGFloat browserHeight = CGRectGetHeight(self.view.bounds)- itemHeight;
+    CGFloat browserHeight = CGRectGetHeight(self.view.bounds)- itemHeight-itemHeight;
+    CGFloat buttonWidth = CGRectGetWidth(self.view.bounds) / 4;
     
     //now, assign the frames
     self.textField.frame = CGRectMake(0, 0, width, itemHeight);
     self.webview.frame = CGRectMake(0, CGRectGetMaxY(self.textField.frame), width, browserHeight);
+    
+    CGFloat currentButtonX = 0;
+    
+    for (UIButton *thisButton in @[self.backButton, self.forwardButton, self.stopButton, self.reloadButton]) {
+        thisButton.frame = CGRectMake(currentButtonX, CGRectGetMaxY(self.webview.frame), buttonWidth, itemHeight);
+        currentButtonX += buttonWidth;
+    }
 }
+
+
+#pragma mark - UITextFieldDelegate
+
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField{
     [textField resignFirstResponder];
@@ -66,11 +118,20 @@
     
     NSURL *URL = [NSURL URLWithString:URLString];
     
+    if ([URLString containsString:@" "]) {
+        URLString = [URLString stringByReplacingOccurrencesOfString:@" "
+                                                         withString:@"+"];
+        NSString *googleString = [[NSString alloc] initWithFormat: @"www.google.com/search?q=%@", URLString];
+        URLString = googleString;
+        NSLog(@"%@",URLString);
+        
+    }
+    NSLog(@"%@", URLString);
     if (!URL.scheme) {
         //the user didn't type http: or https:
         URL = [NSURL URLWithString:[NSString stringWithFormat:@"http://%@", URLString]];
     }
-    
+    NSLog(@"%@", URLString);
     if (URL) {
         NSURLRequest *request = [NSURLRequest requestWithURL:URL];
         [self.webview loadRequest:request];
@@ -78,5 +139,51 @@
     return NO;
 }
 
+
+#pragma mark - UIWebViewDelegate
+
+- (void) webViewDidStartLoad:(UIWebView *)webView {
+    self.frameCount++;
+    [self updateButtonsAndTitle];
+}
+
+- (void) webViewDidFinishLoad:(UIWebView *)webView {
+    self.frameCount--;
+    [self updateButtonsAndTitle];
+}
+
+
+-(void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error{
+    //This if statement is missing from the tutorial. Added it so that the updateButtonsAndTitle will fit properly.
+    if (error.code != -999) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Error", @"Error") message:[error localizedDescription] delegate:nil cancelButtonTitle:NSLocalizedString(@"OK", nil) otherButtonTitles:nil];
+        [alert show];
+    }
+    [self updateButtonsAndTitle];
+    self.frameCount--;
+}
+
+#pragma mark - Miscellaneous
+
+- (void) updateButtonsAndTitle {
+    NSString *webpageTitle = [self.webview stringByEvaluatingJavaScriptFromString:@"document.title"];
+    
+    if (webpageTitle) {
+        self.title = webpageTitle;
+    } else {
+        self.title = self.webview.request.URL.absoluteString;
+    }
+    
+    if (self.frameCount > 0) {
+        [self.activityIndicator startAnimating];
+    } else {
+        [self.activityIndicator stopAnimating];
+    }
+    
+    self.backButton.enabled = [self.webview canGoBack];
+    self.forwardButton.enabled = [self.webview canGoForward];
+    self.stopButton.enabled = self.frameCount > 0;
+    self.reloadButton.enabled = !self.frameCount ==0;
+}
 
 @end
